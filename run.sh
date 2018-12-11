0939d768cdd1d311e17e681b5b61560b2b1b92bf
Amanda Swearngin <amaswea@cs.washington.edu>
2018-12-11 10:18:51 -0800

    Fix proximity/distribution constraints to correct visually separate groups in a container and updated start scripts

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export FLASK_APP=server.py
+export FLASK_DEBUG=1
+flask run
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # Start up the Spectrum project at http://localhost:5000
-ttab -a Terminal "source scout/bin/activate; cd server; ./run_continuously.sh"
+ttab -a Terminal "source scout/bin/activate; ./run_continuously.sh"
 ttab -a Terminal "cd static; npm run watch"
-ttab -a Terminal "cd server; tail -f server.out"
+ttab -a Terminal "tail -f server.out"
 
 
